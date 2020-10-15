e7c9c3879896ac8959c8adb4452d3696f89bc1b1
Alice Freda <alicefreda@gmail.com>
2020-10-15 14:33:46 -0700

    reset sandbox for flexbox nav

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const autoprefixer = require('gulp-autoprefixer');
-
-gulp.task('styles', async function() {
-  gulp.src('css/styles.css')
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('build'))
-});
