a54b5bfe2255bbdd1b3bf450a1965fd9a072573b
Alice Freda <alicefreda@gmail.com>
2020-10-15 09:10:05 -0700

    setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('styles', function() {
+    gulp.src('css/styles.css')
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('build'))
+});
