aa182dfd381586c8d9e032a969f0d67320bae8ee
Alice Freda <alicefreda@gmail.com>
2020-10-15 14:30:20 -0700

    try and get gulp 4 to work with gulp 3 tutorial, unsuccessfully

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('styles', function() {
-    gulp.src('css/styles.css')
+gulp.task('styles', async function() {
+  gulp.src('css/styles.css')
     .pipe(autoprefixer())
     .pipe(gulp.dest('build'))
 });
